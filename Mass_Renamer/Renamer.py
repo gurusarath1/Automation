1a83928f43cc53f9207a8bfa2fe469f5ca9ba12d
Guru Sarath <gurusarath1.official@gmail.com>
2019-06-02 13:15:01 +0530

    Bug fixes
    Bug fixes

--- Mass_Renamer/Renamer.py
+++ Mass_Renamer/Renamer.py
@@ -4,7 +4,7 @@
 import os
 
 FolderPath = 'FolderX'
-genericName_File = 'GenericFileName_'
+genericName_File = '__adjh__'
 genericName_Folder = 'GenericFoldername_'
 i = 0 #File counter
 j = 0 #Folder counter 
@@ -14,11 +14,12 @@ for x in os.listdir(FolderPath):
 	splitX = x.split('.') #separate name and extension
 
 	try:
-		extension = '.' + splitX[1]
-		os.rename('FolderX\\' + x, 'FolderX\\' + genericName_File + str(i) + extension)
+		extension = '.' + splitX[-1]
+		k = splitX[1] # Used to create exception
+		os.rename(FolderPath + '\\' + x, FolderPath + '\\' + genericName_File + str(i) + extension)
 		i += 1
 	except:
 		#Any file without an extension is considered a folder
 		extension = ''
-		os.rename('FolderX\\' + x, 'FolderX\\' + genericName_Folder + str(j) + extension)
+		os.rename(FolderPath + '\\' + x, FolderPath + '\\' + genericName_Folder + str(j) + extension)
 		j += 1
